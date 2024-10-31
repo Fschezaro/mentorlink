fd6ebb0e4470d5f12993c1d21387e88e695a4038
Fernando Schezaro <130468900+Fschezaro@users.noreply.github.com>
2024-10-31 07:15:15 -0300

    conversão para -> boostrap
    + tela de login

--- findyourway.php
+++ findyourway.php
@@ -1,41 +1,30 @@
 <!DOCTYPE html>
 <html lang="pt-br">
 
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <title>MentorLink</title>
-    <link rel="stylesheet" href="./css/style.css">
-    <link rel="icon" href="./img/logo-background.png">
-
-</head>
+<?php include './components/head.html';?>
 
 <body>
 <?php include 'components/header.html';?>
 
-    <section class="principal">
-        <div class="section">
-            <div class="section-item">
-            </div>
-        </div>
-    </section>
+<section class="principal">
+
+</section>
 
 <?php require 'components/footer.html';?>
-    <script>
-        window.addEventListener('load', () => {
-            const principal = document.querySelector('.principal');
-            principal.classList.add('shrink');
-            const section = document.querySelector('.section');
-            setTimeout(() => {
-                section.classList.add('visible'); // Adiciona a classe para ativar a animação
-            }, 1000);
-            const sectionitem = document.querySelector('.section-item');
-            setTimeout(() => {
-                sectionitem.classList.add('visible'); // Adiciona a classe para ativar a animação
-            }, 2900);
-        });
-    </script>
+<!--    <script>-->
+<!--        window.addEventListener('load', () => {-->
+<!--            const principal = document.querySelector('.principal');-->
+<!--            principal.classList.add('shrink');-->
+<!--            const section = document.querySelector('.section');-->
+<!--            setTimeout(() => {-->
+<!--                section.classList.add('visible'); // Adiciona a classe para ativar a animação-->
+<!--            }, 1000);-->
+<!--            const sectionitem = document.querySelector('.section-item');-->
+<!--            setTimeout(() => {-->
+<!--                sectionitem.classList.add('visible'); // Adiciona a classe para ativar a animação-->
+<!--            }, 2900);-->
+<!--        });-->
+<!--    </script>-->
 </body>
 
 </html>
--- index.php
+++ index.php
@@ -9,14 +9,15 @@
 
     <section class="principal">
         <div class="principal-content">
-            <h1>MentorLink</h1>
-            <p>Nos te ajudamos a encontrar seu caminho!</p>
-            <a href="findyourway.php" class="btn btn-primary">Saiba mais</a>
+            <h1>Bem vindo</h1>
+            <a href="findyourway.php" class="btn btn-primary my-3">Começar minha jornada!</a>
+            <p>Nos te ajudamos a encontrar seu caminho</p>
         </div>
     </section>
 
     <section id="about" class="about">
-        <h2>Sobre Nós</h2>
+        <a href="login.php" class="btn btn-sm btn-outline-primary mb-4">Ja faço parte do time!</a>
+        <h2>O que é MentorLink?</h2>
         <p>A MentorLink é uma empresa inovadora dedicada a auxiliar profissionais e estudantes a descobrirem sua área
             específica de atuação no vasto campo da tecnologia. Utilizando um sistema avançado de avaliação de perfil,
             que combina análise de habilidades, interesses pessoais e tendências do mercado </p>
