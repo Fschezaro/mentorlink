e5e563044130b59bcf24b38a1cd154b4566e0ce0
Fernando Schezaro <130468900+Fschezaro@users.noreply.github.com>
2024-11-05 18:01:10 -0300

    Criação da pagina profile

--- index.php
+++ index.php
@@ -3,27 +3,26 @@
 
 <?php require 'components/head.html'; ?>
 
-<body>
+<body class="d-flex flex-column min-vh-100">
 
     <?php include 'components/header.html';?>
+    <main>
+        <section class="principal">
+            <div class="principal-content">
+                <h1>Bem vindo</h1>
+                <a href="form.php" class="btn btn-primary my-3">Começar minha jornada!</a>
+                <p class="mx-3">MentorLink, ajudando você a encontrar seu caminho.</p>
+            </div>
+        </section>
+        <section id="about" class="about">
+            <h2>O que é MentorLink?</h2>
+            <p>A MentorLink é uma empresa inovadora dedicada a auxiliar profissionais e estudantes a descobrirem sua área
+                específica de atuação no vasto campo da tecnologia. Utilizando um sistema avançado de avaliação de perfil,
+                que combina análise de habilidades, interesses pessoais e tendências do mercado </p>
+        </section>
 
-    <section class="principal">
-        <div class="principal-content">
-            <h1>Bem vindo</h1>
-            <a href="form.php" class="btn btn-primary my-3">Começar minha jornada!</a>
-            <p>Nos te ajudamos a encontrar seu caminho</p>
-        </div>
-    </section>
-
-    <section id="about" class="about">
-<!--        <a href="login.php" class="btn btn-sm btn-outline-primary mb-4">Ja faço parte do time!</a>-->
-        <h2>O que é MentorLink?</h2>
-        <p>A MentorLink é uma empresa inovadora dedicada a auxiliar profissionais e estudantes a descobrirem sua área
-            específica de atuação no vasto campo da tecnologia. Utilizando um sistema avançado de avaliação de perfil,
-            que combina análise de habilidades, interesses pessoais e tendências do mercado </p>
-    </section>
-
-    <?php require 'components/footer.html';?>
+        <?php require 'components/footer.html';?>
+    </main>
 </body>
 
 </html>
--- profile.php
+++ profile.php
@@ -4,12 +4,29 @@
 <?php include './components/head.html';?>
 
 <body class="d-flex flex-column min-vh-100">
-<?php include 'components/loggedHeader.html';?>
-<main>
-
-
-</main>
-<?php require 'components/footer.html';?>
+    <?php include 'components/loggedHeader.html';?>
+    <main>
+        <div class="card m-3 shadow d-md-flex text-center">
+            <div class="col-12 d-flex">
+                <div class="col-4 col-md-2 mx-5 mt-5 d-flex flex-column">
+                    <img class="rounded-circle mb-4"  src="./img/br-author-joaopedro.png">
+                    <h1 class="fs-5">João Mendes</h1>
+                </div>
+                <div class="d-flex flex-column justify-content-center align-items-center mx-auto text-center">
+                    <a class="btn btn-info text-white fw-semibold mb-3" style="background-color: #6528e0; border-color: #6528e0">Seguir</a>
+                    <a class="btn btn-info text-white fw-semibold" style="background-color: #6528e0; border-color: #6528e0">Consultoria</a>
+                </div>
+            </div>
+            <hr class="border  border-2 opacity-100 mx-5" style="">
+            <div  class="my-5 mx-4">
+            <h5>Sobre mim</h5>
+            <p>Sou um profissional com sólida experiência em redes de computadores, apaixonado por garantir que as conexões
+                funcionem de forma rápida e segura. Ao longo dos anos, tenho me dedicado a instalar e configurar redes, sempre
+                atento para resolver qualquer problema que apareça e melhorar a comunicação entre os sistemas.</p>
+            </div>
+        </div>
+    </main>
+    <?php require 'components/footer.html';?>
 </body>
 
 </html>
