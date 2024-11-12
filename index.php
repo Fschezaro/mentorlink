b0f0fc3512225274107e93a7157a0112ad866264
Fschezaro <130468900+Fschezaro@users.noreply.github.com>
2024-11-12 12:52:51 -0300

    Ajustes finais

--- index.php
+++ index.php
@@ -3,27 +3,22 @@
 <html lang="pt-br">
 
 <?php require 'components/head.html'; ?>
-
-<body class="d-flex flex-column min-vh-100">
-
+<body class="d-flex flex-column">
     <?php include 'components/header.html';?>
-    <main>
-        <section class="principal">
-            <div class="principal-content">
-                <h1>Bem vindo</h1>
-                <a href="preform.php" class="btn btn-primary my-3">Começar minha jornada!</a>
-                <p class="mx-3">MentorLink, ajudando você a encontrar seu caminho.</p>
-            </div>
-        </section>
-        <section id="about" class="about">
-            <h2>O que é MentorLink?</h2>
-            <p>A MentorLink é uma empresa inovadora dedicada a auxiliar profissionais e estudantes a descobrirem sua área
-                específica de atuação no vasto campo da tecnologia. Utilizando um sistema avançado de avaliação de perfil,
-                que combina análise de habilidades, interesses pessoais e tendências do mercado </p>
-        </section>
-
-        <?php require 'components/footer.html';?>
-    </main>
+    <section class="principal">
+        <div class="principal-content">
+            <h1>Bem vindo</h1>
+            <a href="preform.php" class="btn btn-primary my-3">Começar minha jornada!</a>
+            <p class="mx-3">MentorLink, ajudando você a encontrar seu caminho.</p>
+        </div>
+    </section>
+    <section id="about" class="about h-50">
+        <h2>O que é MentorLink?</h2>
+        <p>A MentorLink é uma empresa inovadora dedicada a auxiliar profissionais e estudantes a descobrirem sua área
+            específica de atuação no vasto campo da tecnologia. Utilizando um sistema avançado de avaliação de perfil,
+            que combina análise de habilidades, interesses pessoais e tendências do mercado </p>
+    </section>
+    <?php require 'components/footer.html';?>
 </body>
 
 </html>
--- result.php
+++ result.php
@@ -4,6 +4,7 @@ if (!isset($_GET['area'])){
 }
 $area = $_GET['area'];
 if ($area == "desenvolvimento"){
+    $area = "Desenvolvimento de softwares";
     $subarea = [
         1 => "Desenvolvimento web",
         2 => "Desenvolvimento Mobile",
@@ -11,14 +12,17 @@ if ($area == "desenvolvimento"){
     ];
     $professional1 = [
         "name" => "Matheus Camargo",
-        "photo" => "img/matheus-camargo.png"
+        "photo" => "img/matheus-camargo.png",
+        "job" => "Engenheiro de software"
     ];
     $professional2 = [
         "name" => "Ana Mônica",
-        "photo" => "img/ana-monica.png"
+        "photo" => "img/ana-monica.png",
+        "job" => "Engenheira de software"
     ];
 
 } elseif ($area == "redes"){
+    $area = "Redes de computadores";
     $subarea = [
         1 => "Administração de Redes",
         2 => "Segurança de Redes",
@@ -26,13 +30,16 @@ if ($area == "desenvolvimento"){
     ];
     $professional1 = [
         "name" => "Bernardo Carvalho",
-        "photo" => "img/bernardo-carvalho.png"
+        "photo" => "img/bernardo-carvalho.png",
+        "job" => "Analista de redes"
     ];
     $professional2 = [
         "name" => "Ricardo Santos",
-        "photo" => "img/ricardo-santos.png"
+        "photo" => "img/ricardo-santos.png",
+        "job" => "Analista de redes"
     ];
 } else {
+    $area = "Ciência de dados";
     $subarea = [
         1 => "Ciência de Dados",
         2 => "Engenharia de Dados",
@@ -40,11 +47,13 @@ if ($area == "desenvolvimento"){
     ];
     $professional1 = [
         "name" => "julio Silveira",
-        "photo" => "img/julio-silveira.png"
+        "photo" => "img/julio-silveira.png",
+        "job" => "Cientista de dados"
     ];
     $professional2 = [
         "name" => "Amanda Santana",
-        "photo" => "img/amanda-santana.png"
+        "photo" => "img/amanda-santana.png",
+        "job" => "Cientista de dados"
     ];
 }
 
@@ -53,31 +62,37 @@ include 'components/loggedHeader.html';
 ?>
 
 <div class="card shadow p-2 m-2 mt-4 text-center">
+    <div class="shadow rounded text-center bg-primary-subtle text-primary-emphasis mb-3 mt-2">
+        <p class="m-3 fs-2"><?= $area; ?></p>
+    </div>
     <div>
         <p class="fw-semibold fs-5">Subáreas que recomendamos para você </p>
-        <div class="mb-3 rounded-pill text-center bg-primary-subtle text-primary-emphasis">
+        <div class="mb-3 mx-4 shadow rounded-pill text-center bg-primary-subtle text-primary-emphasis">
             <p class="p-3">1º  <?= $subarea[1] ?></p>
         </div>
-        <div class="mb-3 rounded-pill text-center bg-primary-subtle text-primary-emphasis">
+        <div class="mb-3 mx-4 shadow rounded-pill text-center bg-primary-subtle text-primary-emphasis">
             <p class="p-3">2º  <?= $subarea[2] ?></p>
         </div>
-        <div class="rounded-pill text-center bg-primary-subtle text-primary-emphasis">
+        <div class="mx-4 shadow rounded-pill text-center bg-primary-subtle text-primary-emphasis">
             <p class="p-3">3º  <?= $subarea[3] ?></p>
         </div>
     </div>
+
     <div class="d-flex flex-column justify-content-center align-items-center mt-4">
         <p class="fw-semibold fs-4">Profissionais especialistas da área</p>
         <div class="card d-flex align-items-center shadow mb-4 p-3 w-75 h-50">
             <a href="<?php echo "profile.php?name=".$professional1['name']; ?>" style="text-decoration: none">
-            <img class="rounded-circle" src="<?= $professional1['photo']; ?>" width="40%">
-            <p class="text-black fs-5 m-2"><?= $professional1['name']; ?></p>
+                <img class="rounded-circle" src="<?= $professional1['photo']; ?>" width="40%">
+                <p class="text-black fs-5 m-2"><?= $professional1['name']; ?></p>
+                <span class="badge text-bg-secondary"> <?= $professional1['job']; ?></span>
             </a>
         </div>
 
         <div class="card d-flex align-items-center shadow mb-4 p-3 w-75 h-50">
             <a href="<?php echo "profile.php?name=".$professional2['name']; ?>" style="text-decoration: none">
-            <img class="rounded-circle" src="<?= $professional2['photo']; ?>" width="40%">
-            <p class="text-black fs-5 m-2"><?= $professional2['name']; ?></p>
+                <img class="rounded-circle" src="<?= $professional2['photo']; ?>" width="40%">
+                <p class="text-black fs-5 m-2"><?= $professional2['name']; ?></p>
+                <span class="badge text-bg-secondary"> <?= $professional2['job']; ?></span>
             </a>
         </div>
     </div>
